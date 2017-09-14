31d49142ab1a9699b6bac26da99ece5ae633bbed
nsnguyen <nsnguyen19@gmail.com>
2017-09-14 01:21:05 -0700

    other load_data

---
+++ load_data_other.sql
@@ -0,0 +1,21 @@
+
+-- psql -f load_data.sql ironfin_development
+
+copy movies from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/movie.del' with (FORMAT csv);
+
+copy actors from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/actor1.del' with (FORMAT csv);
+
+copy actors from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/actor2.del' with (FORMAT csv);
+
+copy actors from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/actor3.del' with (FORMAT csv);
+
+copy directors from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/director.del' with (FORMAT csv);
+
+copy movie_actors from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/movieactor1.del' with (FORMAT csv);
+
+copy movie_actors from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/movieactor2.del' with (FORMAT csv);
+
+copy movie_directors from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/moviedirector.del' with (FORMAT csv);
+
+copy movie_genres from '/Users/nsnguyen/Documents/_Git/IronFin/db/raw_data/moviegenre.del' with (FORMAT csv);
+
