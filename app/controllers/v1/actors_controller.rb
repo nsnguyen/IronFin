2eb6dd7a8c2fdd9de85159875e4e47de864345ad
nnguyen <nnguyen@alg.com>
2017-09-17 14:28:03 -0700

    Add actors validation

--- app/controllers/v1/actors_controller.rb
+++ app/controllers/v1/actors_controller.rb
@@ -3,15 +3,33 @@
 module V1
   class ActorsController < ::V1::ApiController
     def index
-      # definitely need some kind of validations..
 
-      result = ActorServices::Index.new(first_name: params[:first],
-                                        last_name: params[:last],
-                                        sex: params[:sex],
-                                        date_of_birth: params[:dob],
-                                        date_of_death: params[:dod]).run
+      schema = Dry::Validation.Schema do
+        required(:first).filled(:str?, format?: Regexp.new('^[a-zA-Z-. ]*$'))
+        required(:last).filled(:str?)
+      end
 
-      render json: V1::Actor::IndexSerializer.format_json(result)
+      params_check = schema.call(params)
+
+      if params_check.failure?
+        render json: params_check.messages(full: true)
+
+      else
+        result = ActorServices::Index.new(first_name: params[:first],
+                                          last_name: params[:last],
+                                          sex: params[:sex],
+                                          date_of_birth: params[:dob],
+                                          date_of_death: params[:dod]).run
+
+        render json: V1::Actor::IndexSerializer.format_json(result)
+
+      end
+
+
+
+
+
+
 
     end
   end
