521b10cf2f3bfd32374260877647fa2ff38221dc
Administrator <jennifer.lauren.macdonald@gmail.com>
2017-09-23 23:39:30 -0700

    Added FillMovies

---
+++ app/services/actor_services/fill_movies.rb
@@ -0,0 +1,61 @@
+module ActorServices
+  class FillMovies
+
+    attr_accessor :actor_hashes, :actor_ids
+
+    def initialize(actor_hashes, actor_ids)
+      self.actor_hashes = actor_hashes
+      self.actor_ids = actor_ids
+    end
+
+    def run
+      return nil unless actor_hashes.any?
+
+      actors_dict = build_actors_dictionary
+
+      actor_hashes.each do |actor_hash|
+        aid = actor_hash['id']
+        actor_hash['movie'] = actors_dict[aid] || []
+      end
+    end
+
+    # This is to build the query dictionary. {key, value}
+    def build_actors_dictionary
+      dict = {}
+
+      movies.each do |movie|
+        key = movie['aid']
+
+        # if key does not exist, set to empty array.
+        dict[key] ||= []
+
+        # push to that key.
+        dict[key].push as_hashes(movie)
+      end
+
+      dict
+    end
+
+    def as_hashes(movie)
+      {
+          'aid' => movie['aid'],
+          'movie' => movie['movie']
+      }
+    end
+
+    # actor_ids is an array of actor_id. This is one time query call.
+    def query
+      query = <<~HEREDOC
+        SELECT *
+        FROM actor_movies am
+        WHERE am.aid in (?)
+      HEREDOC
+
+      ActiveRecord::Base.send(:sanitize_sql, [query, actor_ids])
+    end
+
+    def movies
+      @movies ||= ActiveRecord::Base.connection.execute(query)
+    end
+  end
+end
--- app/services/actor_services/index.rb
+++ app/services/actor_services/index.rb
@@ -2,23 +2,55 @@
 
 module ActorServices
   class Index
-    attr_accessor :first_name , :last_name, :sex, :date_of_birth, :date_of_death
+    attr_accessor :first_name , :last_name, :sex, :date_of_birth, :date_of_death, :movies_acted_in
 
-    def initialize(first_name: nil, last_name: nil, sex: nil, date_of_birth: nil, date_of_death: nil)
+    def initialize(first_name: nil, last_name: nil, sex: nil, date_of_birth: nil, date_of_death: nil, movies_acted_in: nil)
       self.first_name = first_name
       self.last_name = last_name
       self.sex = sex
       self.date_of_birth = date_of_birth
       self.date_of_death = date_of_death
+      self.movies_acted_in = movies_acted_in
 
     end
 
     def run
-      run_query
+      # get initial actors data in PDG
+      actors = run_query
+
+      # build actors_ids array so it can pass into second SQL query to search for genres.
+      actor_ids = actor_ids(actors)
+
+      # convert to hashes so we can add genres in easily.
+      actor_hashes = as_hashes(actors)
+
+      # pass actors_hashes as reference. Fill Movies class will append actors genres.
+      ActorServices::FillMovies.new(actor_hashes, actor_ids).run
+
+      actor_hashes
+    end
+
+    def actor_ids(actors)
+      # this is to put actor_ids into an array
+      actors.pluck('id')
     end
 
     def run_query
       @run_query ||= ActiveRecord::Base.connection.execute(query)
+    end
+
+    def as_hashes(actors)
+      actors.map do |actor|
+      {
+          'id' => actor['id'],
+          'first_name' => actor['first_name'],
+          'last_name' => actor['last_name'],
+          'sex' => actor['sex'],
+          'date_of_birth' => actor['date_of_birth'],
+          'date_of_death' => actor['date_of_death'],
+          'movies_acted_in' => []
+      }
+      end
     end
 
     # probably a good idea to use ActiveRecord but let's just do SQL query to show what we can do.
@@ -26,8 +58,6 @@ module ActorServices
       query = <<~HEREDOC
         SELECT *
         FROM Actors a
-        INNER JOIN movie_actors ma ON a.id = ma.aid
-        INNER JOIN movies m on ma.mid = m.id
         WHERE (LOWER(a.first) LIKE LOWER(?) OR LOWER(?) IS NULL)
         AND (LOWER(a.last) LIKE LOWER(?) OR LOWER(?) IS NULL)
         AND (LOWER(a.sex) = LOWER(?) OR LOWER(?) IS NULL)
