c151bbea233c7435baaacdd1ad2e348b027a2772
Eezze <nsnguyen19@gmail.com>
2017-09-27 01:04:11 -0700

    fixed review serializer

--- app/serializers/v1/Review/index_serializer.rb
+++ app/serializers/v1/Review/index_serializer.rb
@@ -7,18 +7,14 @@ module V1
       def self.format_json(reviews)
         reviews.map do |review|
           {
-              firstName: actor['first'],
-              lastName: actor['last'],
-              gender: actor['sex'],
-              dateOfBirth: actor['dob'],
-              dateOfDeath: actor['dod'],
-              reviewerName: review['name'],
-              timeOfReview: review['time'],
-              movieId: review['mid'],
-              reviewersRatingOfMovie: review['rating'],
-              commentsAboutMovie: review['comment']
+              id: review['id'],
+              name: review['name'],
+              time: review['time'],
+              rating: review['rating'],
+              comment: review['comment']
           }
         end
       end
     end
   end
+end
